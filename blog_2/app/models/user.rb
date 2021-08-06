5065f41afb27a5155f5e6f495323664f608b3f9b
Dylan-Barclay <dr.barclay99@gmail.com>
2021-08-06 15:05:27 -0500

    s

--- blog_2/app/models/user.rb
+++ blog_2/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
     validates :first_name, :last_name, :email_address, presence: true
     has_many :owners, dependent: :destroy
     has_many :messages, dependent: :destroy
+    has_many :posts
+    has_many :blogs, through: :owners
 end
